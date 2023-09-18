25c5196f933f94c4a91f033224fbf865b0604bf5
jiho <unit013@naver.com>
2023-09-18 23:00:29 +0900

    함수 구현

--- src/MOKEngine.sol
+++ src/MOKEngine.sol
@@ -270,7 +270,9 @@ contract MOKEngine is ReentrancyGuard
         }
         _revertIfHealthFactorIsBroken(msg.sender);
     }
-    function getHealthFactor() external view {}
+    function getHealthFactor(address user) external view returns (uint256) {
+        return _healthFactor(user);
+    }
 
     //////////////////////////////////
     // Private & Internal Functions //
